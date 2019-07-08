74037644abc60b941836f6ce515b9a9ccd57f562
zhongqi <imzhongqi@gmail.com>
2019-07-08 11:59:10 +0800

    add default http controller

---
+++ App/HttpController/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\HttpController;
+
+use EasySwoole\Http\AbstractInterface\Controller;
+
+class Index extends Controller {
+    function index() {
+        $this->response()->write('hello,worold');
+    }
+}
